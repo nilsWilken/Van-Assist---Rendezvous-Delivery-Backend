46b1f1ce36084af9a6485128d4820594b132de29
Nils Wilken <wilken@es.uni-mannheim.de>
2020-01-29 16:59:15 +0100

    Python prepare script now deletes unused docker images

--- prepare_vanassistwebservice_docker.py
+++ prepare_vanassistwebservice_docker.py
@@ -4,7 +4,6 @@ client = docker.from_env()
 
 containers = client.containers.list(all="true", filters={"ancestor": "vanassistwebservice:1.0"})
 for container in containers:
-    print(container.image)
     container.stop()
     container.remove()
 
@@ -17,14 +16,17 @@ images = client.images.list()
 for img in images:
     if len(img.tags) < 1:
         continue
-    if img.tags[0] == "vanassistubuntu:1.0":
+    elif img.tags[0] == "vanassistubuntu:1.0":
         vanassistubuntu_found = True
-        break
 
 if vanassistubuntu_found == False:
-    print("False")
     client.images.build(path="./VanAssistUbuntu", tag="vanassistubuntu:1.0", rm="true")
 
 client.images.build(path=".", tag="vanassistwebservice:1.0", rm="true")
 
 client.containers.run(image="vanassistwebservice:1.0", ports={"8000/tcp":"8000"}, detach="false")
+
+images = client.images.list()
+for img in images:
+    if len(img.tags) < 1:
+        client.images.remove(image=img.id)
